403618d9b3d198b16edf81702458aac2d226f8ce
Luis <luismiguelslancastre@gmail.com>
2020-05-21 14:07:08 +0100

    functions + arrow functions

---
+++ 3 Javascript/3 functions + arrow functions.js
@@ -0,0 +1,32 @@
+//-------------------------FUNCTIONS-----------------------------
+
+function soma() {
+  console.log(5 + 5);
+
+}
+
+soma()
+
+function multiplicaçao(a, b) {
+  console.log(a * b);
+
+}
+
+multiplicaçao(8, 7)
+
+
+//---------------------------ARROW FUNCTIONS----------------------
+
+const mult = () => {
+  console.log(10 * 10);
+
+}
+
+mult()
+
+const som = (a, b) => {
+  console.log(a + b);
+
+}
+
+som(10, 10)
